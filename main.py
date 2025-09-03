bcad06ebc1356ecf16f269e377c2006c4d822b65
Amboara <amboaraniainarkt@gmail.com>
2025-09-03 08:59:08 +0200

    all

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
 
-@app.get("/ping")
-async def root():
-    return {"message": "pong"}
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+phones_db: List[Phone] = []
+
+
+@app.get("/health")
+def health_check():
+    return "Ok"
+
+
+@app.post("/phones", status_code=status.HTTP_201_CREATED)
+def create_phones(new_phones: List[Phone]):
+    global phones_db
+    phones_db.extend(new_phones)
+    return {"message": f"{len(new_phones)} phone ajouté"}
+
+
+@app.get("/phones", response_model=List[Phone])
+def get_phones():
+    return phones_db
+
+
+@app.get("/phones/{id}", response_model=Phone)
+def get_phone(id: str):
+    for phone in phones_db:
+        if phone.identifier == id:
+            return phone
+    raise HTTPException(
+        status_code=404,detail=f"Phone avec l'identifiant '{id}' n'existe pas."
+    )
+
+
+@app.put("/phones/{id}/characteristics", response_model=Phone)
+def update_characteristics(id: str, new_characteristics:
